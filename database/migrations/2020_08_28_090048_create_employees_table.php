a6d3429e0335cacbb9580b302ce1f7b25f783d38
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-08-28 17:53:35 +0800

    initial commit

---
+++ database/migrations/2020_08_28_090048_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('employee_type_id')->nullable()->default(12);
+            $table->string('name', 50);
+            $table->string('nip', 18);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
