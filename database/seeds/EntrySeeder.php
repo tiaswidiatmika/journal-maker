4d8e3cd918fa274e142097c0de6fbbe13fb8ea50
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-08-28 22:29:24 +0800

    seeds entries table

--- database/seeds/EntrySeeder.php
+++ database/seeds/EntrySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EntrySeeder extends Seeder
 {
@@ -11,6 +12,23 @@ class EntrySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $entries = array(
+            1 => [
+                'Memastikan mesin autogate berfungsi dengan baik, menyiapkan dan merapikan line antrian serta membantu petugas counter yang membutuhkan bantuan.',
+                "Mengkomunikasikan kepada penumpang untuk mempersiapkan dokumen keimigrasian untuk memperlancar pelayanan",
+            ],
+            2 => [
+                'Memperhatikan dan mencatat semua bahan yang dibutuhkan setiap counter dalam melakukan pemeriksaan keimigrasian agar berjalan dengan lancar',
+                'Memperhatikan dan mencatat semua bahan yang dibutuhkan setiap counter dalam melakukan pemeriksaan keimigrasian agar berjalan dengan lancar'
+            ],  
+        );
+
+        foreach ($entries as $key => $value) {
+            foreach ($value as $entry) {
+                DB::table('entries')->insert([
+                    'entry_desc' => $entry,
+                ]);
+            }
+        }
     }
 }
