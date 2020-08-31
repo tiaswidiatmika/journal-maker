471c72fc16df579eca2041471659b04a9303a21f
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-08-31 21:54:40 +0800

    add route method to show attached entries employee has

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/task', function(Request $request){
+Route::get('/task', function(Request $request){
     $employeeId = $request->input('employee_id');
     $entry = $request->input('entries');
 
