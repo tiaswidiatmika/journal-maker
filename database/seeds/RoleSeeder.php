432a7a351a03f429b56f040728c8fd546cf77256
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-08-31 21:53:01 +0800

    seed roles table

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -11,6 +11,26 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // RoleSeeder will seed both 'roles table' and 'entries table' simultaneously
+        $calonJftRoles = [
+            'Menyiapkan bahan perlengkapan yang diperlukan untuk melancarkan kegiatan pemeriksaan keimigrasian di Tempat Pemeriksaan Imigrasi',
+            'Mencatat kebutuhan bahan dalam kegiatan pemeriksaan keimigrasian di Tempat Pemeriksaan Imigrasi',
+            'Mencatat permasalahan dalam kegiatan pemeriksaan keimigrasian di Tempat Pemeriksaan Imigrasi',
+            'Mencatat kegiatan pemeriksaan keimigrasian di Tempat Pemeriksaan Imigrasi yang telah selesai dilaksanakan',
+            'Mencatat pengaduan dalam pemeriksaan keimigrasian di Tempat Pemeriksaan Imigrasi',
+            'Melaksanakan penertiban antrean pemeriksaan keimigrasian di Tempat Pemeriksaan Imigrasi dan atau Area Imigrasi',
+            'Menyiapkan bahan laporan keberangkatan dan atau kedatangan penumpang di Tempat Pemeriksaan Imigrasi',
+            'Menyiapkan bahan tindakan keimigrasian pada Tempat Pemeriksaan Imigrasi',
+            'Membuat laporan pelaksanaan tugas harian',
+        ];
+
+        foreach ($calonJftRoles as $key => $value) {
+            DB::table('roles')->insert([
+                'role_desc' => $value,
+            ]);    
+        }
+
+
+        
     }
 }
