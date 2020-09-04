bc3c1abd0c539258fd5a727e64ce490beae63c10
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-09-04 22:05:44 +0800

    seed new user: tias

--- database/seeds/EmployeeSeeder.php
+++ database/seeds/EmployeeSeeder.php
@@ -11,6 +11,20 @@ class EmployeeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $employees = [
+            [
+                'name' => 'tias widiatmika',
+                'employee_type_id' => 1,
+                'nip' => '199105082017121001',
+            ],
+        ];
+        
+        foreach ($employees as $employee) {
+            DB::table('employees')->insert([
+                'employee_type_id' => $employee['employee_type_id'],
+                'name' => $employee['name'],
+                'nip' => $employee['nip'],
+            ]);
+        }
     }
 }
