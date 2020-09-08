c25de72ca433ada7061e5a217e8e9909a18be95f
Tias Widiatmika <tiaswidiatmika@gmail.com>
2020-09-08 23:16:58 +0800

    change relationship

--- app/Role.php
+++ app/Role.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    public function employeeTypes()
+    public function employeeType()
     {
         return $this->belongsTo('App\EmployeeType');
     }
